fe62a960fe5f948788c622dbac8ba0ff420ed1c1
HuberAdrian <huberadrian@web.de>
2023-03-26 12:53:06 +0200

    working buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,11 +18,11 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>
-          {!user && (<>
+          {!user.isSignedIn && (<>
             <h1> Sign in </h1>
               <SignInButton />
           </>)}
-          {!!user && (<>
+          {!!user.isSignedIn && (<>
             <h1> Sign out </h1>
             <SignOutButton />
           </>)}
