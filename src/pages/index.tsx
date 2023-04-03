047ae3ab55951d206f8845e084d7da67d8adbdf7
HuberAdrian <huberadrian@web.de>
2023-04-03 22:28:29 +0200

    font-sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,7 +57,7 @@ const PostView = (props: PostWithUser) => {
               <span>{`@${author.firstName}`}</span>
               <span className="front-thin" >{`• ${dayjs(post.createdAt).fromNow()}`}</span>
             </div>
-            <span>{post.content}</span>
+            <span className="text-2xl" >{post.content}</span>
           </div>
       </div>
     </>
