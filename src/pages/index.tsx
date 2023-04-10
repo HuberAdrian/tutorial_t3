ed9ef178303251a3f06d1bec225e35a372cd09d9
HuberAdrian <huberadrian@web.de>
2023-04-10 19:12:04 +0200

    Posting loading state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ import Image from "next/image";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 
 import { useState } from "react";
 import { toast } from "react-hot-toast";
@@ -67,9 +67,27 @@ const CreatePostWizard = () => {
         value={input}
         onChange={(e) => setInput(e.target.value)}
         disabled={isPosting}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault() ;
+            if (input !== "") {
+              mutate({content: input});
+            }
+            }
+          }
+        }
       />
+      {input !== "" && !isPosting && ( 
       <button
-        onClick={() => mutate({content: input})}> Post </button>
+        onClick={() => mutate({content: input})} > Post 
+        </button>
+      )}
+      {/* show loading spinner when posted */}
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
     </div>
   );
 };
